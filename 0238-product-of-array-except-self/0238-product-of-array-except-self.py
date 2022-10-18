f91015950ccfdb39418bbc4adea6f18e4672ff8e
Fekede Alemayehu <88454240+abudah@users.noreply.github.com>
2022-10-18 22:40:49 +0300

    Time: 239 ms (93.90%), Space: 21.2 MB (82.07%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -9,16 +9,4 @@ class Solution:
         for j in range(len(nums)-1,-1,-1):
             answer[j]= answer[j]*post
             post=post*nums[j]
-           
-        
-        # for i in range(len(nums)-2,-1,-1):
-        #     postfix.append(postfix[-1]*nums[i])
-        # postfix=postfix[::-1]
-        # for i in range(len(nums)):
-        #     if i==0:
-        #         answer.append(postfix[1])
-        #     elif i==len(nums)-1:
-        #         answer.append(prefix[len(nums)-2])
-        #     else:
-        #         answer.append(prefix[i-1]*postfix[i+1])
         return answer
