9ed2cab6fcc4f6c8208ef33897eeae6ef7f8f67c
Fekede Alemayehu <88454240+abudah@users.noreply.github.com>
2022-10-18 22:40:26 +0300

    Time: 242 ms (92.41%), Space: 21.2 MB (82.07%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,18 +1,24 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        answer=[]
-        prefix=[nums[0]]
-        postfix=[nums[-1]]
-        for i in range(1,len(nums)):
-            prefix.append(prefix[-1]*nums[i])
-        for i in range(len(nums)-2,-1,-1):
-            postfix.append(postfix[-1]*nums[i])
-        postfix=postfix[::-1]
-        for i in range(len(nums)):
-            if i==0:
-                answer.append(postfix[1])
-            elif i==len(nums)-1:
-                answer.append(prefix[len(nums)-2])
-            else:
-                answer.append(prefix[i-1]*postfix[i+1])
+        answer=[1]
+        pre=1
+        post=1
+        for i in range(0,len(nums)-1):
+            pre=pre*nums[i]
+            answer.append(pre)
+        for j in range(len(nums)-1,-1,-1):
+            answer[j]= answer[j]*post
+            post=post*nums[j]
+           
+        
+        # for i in range(len(nums)-2,-1,-1):
+        #     postfix.append(postfix[-1]*nums[i])
+        # postfix=postfix[::-1]
+        # for i in range(len(nums)):
+        #     if i==0:
+        #         answer.append(postfix[1])
+        #     elif i==len(nums)-1:
+        #         answer.append(prefix[len(nums)-2])
+        #     else:
+        #         answer.append(prefix[i-1]*postfix[i+1])
         return answer
