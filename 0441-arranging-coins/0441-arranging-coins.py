eaada1c7b8cc54b7a3620ddd4a117414e57dc5a1
Fekede Alemayehu <88454240+abudah@users.noreply.github.com>
2022-10-23 16:22:51 +0300

    Time: 1202 ms (32.95%), Space: 13.7 MB (96.19%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,12 +1,10 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        if n==1:
-            return 1
-        i=1
-        sum=0
-        while True:
-            sum=int(i*(i+1)/2)
-            if sum>n:
-                break
+        i=0
+        while n>0:
             i+=1
-        return i-1
+            n-=i
+        if n==0:
+            return i
+        else:
+            return i-1
