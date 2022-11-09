7eba17f17067146a621a2272cd1386bc47aa55de
Fekede Alemayehu <88454240+abudah@users.noreply.github.com>
2022-11-09 06:27:56 +0300

    Time: 92 ms (67.97%), Space: 14.6 MB (82.42%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,9 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         n=len(nums)
-        if target in nums:
-            return nums.index(target)
         for i in range(n):
-            if nums[i]>target:
+            if nums[i]>=target:
                 return i
         return n
