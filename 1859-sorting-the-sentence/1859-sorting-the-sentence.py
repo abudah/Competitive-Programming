dfe43f13662cb141733770def8bdd6a8f18f74ef
Fekede Alemayehu <88454240+abudah@users.noreply.github.com>
2022-11-12 17:45:41 +0300

    Time: 36 ms (87.38%), Space: 13.9 MB (59.03%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -2,10 +2,9 @@ class Solution:
     def sortSentence(self, s: str) -> str:
         def take_end(elem):
             return elem[-1]
-        process_string=s.split(' ')
-        processed_string=sorted(process_string, key=take_end )
-        combined_words=processed_string[0][:-1]
-        for i in range(1,len(processed_string)):
-            combined_words+=' '+processed_string[i][:-1]
-        return combined_words
+        p=sorted(s.split(), key=take_end )
+        c=p[0][:-1]
+        for i in range(1,len(p)):
+            c+=' '+p[i][:-1]
+        return c
             
