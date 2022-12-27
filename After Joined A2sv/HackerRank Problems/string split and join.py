b1cd740025f3004429abda475e5bf00fd2841966
Fekede Alemayehu <fekedealemayehu4@gmail.com>
2022-12-28 00:24:58 +0300

    deleted some hackerrank files

--- After Joined A2sv/HackerRank Problems/string split and join.py
+++
@@ -1,6 +0,0 @@
-def split_and_join(line):
-    # write your code here
-    splitted=line.split(" ")
-    join="-".join(splitted)
-    return join
-if __name__ == '__main__':
--- After Joined A2sv/HackerRank Problems/swap case.py
+++
@@ -1,4 +0,0 @@
-def swap_case(s):
-    return s.swapcase()
-
-if __name__ == '__main__':
